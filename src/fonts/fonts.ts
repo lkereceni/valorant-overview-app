854c7538cc0318bfdf28452f011a648c08ce31ce
lkereceni <lovro.kereceni@gmail.com>
2024-07-23 13:42:53 +0200

    Initial commit

---
+++ src/fonts/fonts.ts
@@ -0,0 +1,11 @@
+import { Montserrat, Oswald } from "next/font/google";
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+export const oswald = Oswald({
+  subsets: ["latin"],
+  variable: "--font-oswald",
+});
